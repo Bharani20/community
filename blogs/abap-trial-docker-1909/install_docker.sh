b0b8a7f2b2c73547abf1ea001fa1b52a1c90e7ca
ameya-sap-gcp <ameya.sap.gcp@gmail.com>
2023-08-23 06:01:17 +0000

    Modifed the install script

--- blogs/abap-trial-docker-1909/install_docker.sh
+++ blogs/abap-trial-docker-1909/install_docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Start Installation"
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
