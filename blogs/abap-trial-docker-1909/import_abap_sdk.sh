c01a304b548bda5795e53f3ac37211611ba10940
ameya-sap <129621732+ameya-sap@users.noreply.github.com>
2023-12-13 19:57:30 +0530

    Update import_abap_sdk.sh

--- blogs/abap-trial-docker-1909/import_abap_sdk.sh
+++ blogs/abap-trial-docker-1909/import_abap_sdk.sh
@@ -8,13 +8,13 @@ wget  https://storage.googleapis.com/cloudsapdeploy/connectors/abapsdk/abap-sdk-
 unzip abap-sdk-for-google-cloud-1.5.zip
 
 #Copy the file to the trans folder of the docker container
-sudo docker cp K900191.GM1 a4h:/usr/sap/trans/cofiles/K900191.GM1
-sudo docker cp R900191.GM1 a4h:/usr/sap/trans/data/R900191.GM1
+sudo docker cp K900191.GM1 a4h:/usr/sap/trans/cofiles/K900219.GM1
+sudo docker cp R900191.GM1 a4h:/usr/sap/trans/data/R900219.GM1
 sudo docker cp K900221.GM1 a4h:/usr/sap/trans/cofiles/K900221.GM1
 sudo docker cp R900221.GM1 a4h:/usr/sap/trans/data/R900221.GM1
 
 #Run the "tp" command to import the transport
-sudo docker exec -it a4h runuser -l a4hadm -c 'tp addtobuffer GM1K900191 A4H client=001 pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL'
-sudo docker exec -it a4h runuser -l a4hadm -c 'tp pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL import GM1K900191 A4H U128 client=001'
+sudo docker exec -it a4h runuser -l a4hadm -c 'tp addtobuffer GM1K900219 A4H client=001 pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL'
+sudo docker exec -it a4h runuser -l a4hadm -c 'tp pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL import GM1K900219 A4H U128 client=001'
 sudo docker exec -it a4h runuser -l a4hadm -c 'tp addtobuffer GM1K900221 A4H client=001 pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL'
 sudo docker exec -it a4h runuser -l a4hadm -c 'tp pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL import GM1K900221 A4H U128 client=001'
