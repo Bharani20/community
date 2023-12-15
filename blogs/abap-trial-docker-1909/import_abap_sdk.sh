b140bf4c9d61e8e21e152616a090e1e3b95fad16
ameya-sap <129621732+ameya-sap@users.noreply.github.com>
2023-12-15 18:01:52 +0530

    Update import_abap_sdk.sh

--- blogs/abap-trial-docker-1909/import_abap_sdk.sh
+++ blogs/abap-trial-docker-1909/import_abap_sdk.sh
@@ -3,15 +3,26 @@
 mkdir abap_sdk_transport
 cd abap_sdk_transport
 wget  https://storage.googleapis.com/cloudsapdeploy/connectors/abapsdk/abap-sdk-for-google-cloud-1.5.zip
+wget  https://storage.googleapis.com/cloudsapdeploy/connectors/abapsdk/hotfix/ABAP_SDK_V1_5_HOTFIX.zip
 
 #Unzip the transport files
 unzip abap-sdk-for-google-cloud-1.5.zip
+unzip ABAP_SDK_V1_5_HOTFIX.zip
 
 #Copy the file to the trans folder of the docker container
 sudo docker cp K900219.GM1 a4h:/usr/sap/trans/cofiles/K900219.GM1
 sudo docker cp R900219.GM1 a4h:/usr/sap/trans/data/R900219.GM1
 sudo docker cp K900221.GM1 a4h:/usr/sap/trans/cofiles/K900221.GM1
 sudo docker cp R900221.GM1 a4h:/usr/sap/trans/data/R900221.GM1
+sudo docker cp K900223.GM1 a4h:/usr/sap/trans/cofiles/K900223.GM1
+sudo docker cp R900223.GM1 a4h:/usr/sap/trans/data/R900223.GM1
+
+#Change owner and permission
+sudo docker exec -it a4h runuser -l root -c 'chmod 777 /usr/sap/trans/cofiles/*'
+sudo docker exec -it a4h runuser -l root -c 'chmod 777 /usr/sap/trans/data/*'
+sudo docker exec -it a4h runuser -l root -c 'chown a4hadm /usr/sap/trans/cofiles/*'
+sudo docker exec -it a4h runuser -l root -c 'chown a4hadm /usr/sap/trans/data/*'
+
 
 #Run the "tp" command to import the transport
 sudo docker exec -it a4h runuser -l a4hadm -c 'tp addtobuffer GM1K900219 A4H client=001 pf=/usr/sap/trans/bin/TP_DOMAIN_A4H.PFL'
